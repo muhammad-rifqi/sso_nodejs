c971b8a13b0bc1c9510f5984d87885bc58a20b47
Muhammad Rifqi <muhammad45rifki@gmail.com>
2025-01-09 11:47:34 +0700

    update

--- db/query.js
+++ db/query.js
@@ -1,11 +1,12 @@
-const md5 = require('md5');
+// const md5 = require('md5');
 const { executeQuery } = require('./postgres');
 //::::::::::::::::::::::::::::::Start Of LOGIN LOGOUT :::::::::::::::::::::::::::::::::::::::::::::::::::::
 const do_login = async (req, res) => {
     const email = req?.body?.email;
     const uri = req.body.url;
-    const password = md5(req?.body?.password);
-    const sql = await executeQuery("SELECT * FROM users where email = $1 AND password =$2 AND users.approve = 'Y' ", [email, password])
+    // const password = md5(req?.body?.password);
+     // password = $2
+    const sql = await executeQuery("SELECT * FROM users where email = $1 AND users.approve = 'Y' ", [email])
     if (sql?.length > 0) {
         const isLogin = true;
         res.cookie("islogin", isLogin, {
@@ -13,7 +14,8 @@ const do_login = async (req, res) => {
             domain: '.rifhandi.com', 
             secure: true,        
             httpOnly: false,     
-            sameSite: 'None',    
+            sameSite: 'None',
+            overwrite: true,    
         });
         res.cookie("id", sql[0]?.id, {
             expires: new Date(Date.now() + 86400000 * 24),
@@ -21,6 +23,7 @@ const do_login = async (req, res) => {
             secure: true,        
             httpOnly: false,     
             sameSite: 'None',  
+            overwrite: true,
         });
         res.cookie("name", sql[0]?.name, {
             expires: new Date(Date.now() + 86400000 * 24),
@@ -28,6 +31,7 @@ const do_login = async (req, res) => {
             secure: true,        
             httpOnly: false,     
             sameSite: 'None',  
+            overwrite: true,
         });
         res.cookie("roles_id", sql[0]?.role_id, {
             expires: new Date(Date.now() + 86400000 * 24),
@@ -35,6 +39,7 @@ const do_login = async (req, res) => {
             secure: true,        
             httpOnly: false,     
             sameSite: 'None',  
+            overwrite: true,
         });
         res.cookie("id_province", sql[0]?.id_province, {
             expires: new Date(Date.now() + 86400000 * 24),
@@ -42,13 +47,15 @@ const do_login = async (req, res) => {
             secure: true,        
             httpOnly: false,     
             sameSite: 'None',  
+            overwrite: true,
         });
         res.cookie("directorat_id", sql[0]?.directorat_id, {
             expires: new Date(Date.now() + 86400000 * 24),
             domain: '.rifhandi.com', 
             secure: true,        
             httpOnly: false,     
-            sameSite: 'None',  
+            sameSite: 'None', 
+            overwrite: true, 
         });
         res.redirect(uri + '/dashboard');
     } else {
@@ -60,14 +67,14 @@ const do_login = async (req, res) => {
 const do_logout = (req, res) => {
 
     const uri_local = "http://localhost:3005";
-    const uri_dev = "http://sso.rifhandi.com";
+    const uri_dev = "https://sso.rifhandi.com";
 
-    res.clearCookie("islogin");
-    res.clearCookie("name");
-    res.clearCookie("id");
-    res.clearCookie("roles_id");
-    res.clearCookie("id_province");
-    res.clearCookie("directorat_id");
+    res.clearCookie("islogin", {domain: ".rifhandi.com"});
+    res.clearCookie("name", {domain: ".rifhandi.com"});
+    res.clearCookie("id", {domain: ".rifhandi.com"});
+    res.clearCookie("roles_id", {domain: ".rifhandi.com"});
+    res.clearCookie("id_province", {domain: ".rifhandi.com"});
+    res.clearCookie("directorat_id", {domain: ".rifhandi.com"});
     res.redirect(uri_dev + '/login');
 }
 
